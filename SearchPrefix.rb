b15ef248471d7ab36f5f266d9e18e23f03322fd7
Ubuntu <ubuntu@ip-172-31-16-247.us-east-2.compute.internal>
2024-02-06 19:02:51 +0000

    Version 1.0

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -1,7 +1,10 @@
 def searchPrefix(arr,s)
 
+	arr.select{ |item| item.start_with?(s)}
+
 end
 
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
 
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
+
