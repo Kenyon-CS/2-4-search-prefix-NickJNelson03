0cde9535b05253394803f47cb3fecf7a02fd558a
Ubuntu <ubuntu@ip-172-31-16-247.us-east-2.compute.internal>
2024-02-07 02:25:55 +0000

    Version 1.1

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -5,6 +5,10 @@ def searchPrefix(arr,s)
 end
 
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
-
+puts ""
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
-
+puts ""
+print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"], "z"))
+puts ""
+print(searchPrefix(["123abc", "456def", "789ghi", "abc123"], "123"))
+puts ""
